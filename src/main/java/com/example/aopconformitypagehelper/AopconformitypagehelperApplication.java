56d39a8357ea96d74c62bf1c673126dafdb0f66a
laiyunxiang <294805447@qq.com>
2018-12-27 16:34:52 +0800

    Initial commit

---
+++ src/main/java/com/example/aopconformitypagehelper/AopconformitypagehelperApplication.java
@@ -0,0 +1,14 @@
+package com.example.aopconformitypagehelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AopconformitypagehelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AopconformitypagehelperApplication.class, args);
+    }
+
+}
+
---
+++ src/test/java/com/example/aopconformitypagehelper/AopconformitypagehelperApplicationTests.java
@@ -0,0 +1,17 @@
+package com.example.aopconformitypagehelper;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AopconformitypagehelperApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
